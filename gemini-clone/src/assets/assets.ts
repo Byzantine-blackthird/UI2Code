5589a90aff3a23fedc91b73ff91a3554182201f6
Baron <979034149@qq.com>
2024-05-24 23:31:33 +0800

    UI create
    create ui

---
+++ gemini-clone/src/assets/assets.ts
@@ -0,0 +1,33 @@
+import bulb_icon from './bulb.svg'
+import code_icon from './code.svg'
+import compass_icon from './compass.svg'
+import gallery_icon from './gallery.svg'
+import gemini_icon from './gemini.svg'
+import history_icon from './history.svg'
+import menu_icon from './menu.svg'
+import message_icon from './message.svg'
+import mic_icon from './mic.svg'
+import plus_icon from './plus.svg'
+import question_icon from './question.svg'
+import send_icon from './Send.svg'
+import setting_icon from './setting.svg'
+import user_icon from './user.svg'
+import youtube_icon from './youtube.svg'
+
+export const assets = {
+    bulb_icon,
+    code_icon,
+    compass_icon,
+    gallery_icon,
+    gemini_icon,
+    history_icon,
+    menu_icon,
+    message_icon,
+    mic_icon,
+    plus_icon,
+    question_icon,
+    send_icon,
+    setting_icon,
+    user_icon,
+    youtube_icon,
+}
